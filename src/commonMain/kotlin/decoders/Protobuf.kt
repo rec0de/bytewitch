9eebba043fda66261181c9f4a47d89f458d83c5d
rec0de <mail@rec0de.net>
2024-06-10 11:32:18 +0200

    fix protobuf offset search on parse exception

--- src/commonMain/kotlin/decoders/Protobuf.kt
+++ src/commonMain/kotlin/decoders/Protobuf.kt
@@ -28,11 +28,11 @@ class ProtobufParser {
                     //Logger.log("divined start offset $effectiveStartOffset")
                     val prefix = data.sliceArray(0 until effectiveStartOffset)
 
-                    val parser = ProtobufParser()
-                    val result = parser.parse(data.fromIndex(effectiveStartOffset), effectiveStartOffset)
-
                     offsetSearchStart += 1
 
+                    val parser = ProtobufParser()
+                    val result = parser.parse(data.fromIndex(effectiveStartOffset), effectiveStartOffset)
+                    
                     Logger.log("Parsed protobuf accounts for ${((parser.offset.toDouble() / data.size)*100).roundToInt()}% of input bytes")
 
                     // parsed protobuf should at least represent 30% of the input bytes
