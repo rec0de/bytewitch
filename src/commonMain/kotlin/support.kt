6850ee4d962daeb62c7ca0b005051ee3a1d6a9ad
Nils Rollshausen <nrollshausen@seemoo.tu-darmstadt.de>
2025-05-20 14:47:01 +0200

    tweak text detection

--- src/commonMain/kotlin/support.kt
+++ src/commonMain/kotlin/support.kt
@@ -29,8 +29,9 @@ fun looksLikeUtf16String(string: String, enableLengthBias: Boolean = true): Doub
 
     // string is mostly printable ASCII, seems plausible
     // avoid two-letter strings with one ascii char getting good ascii percentage by setting "min length" to 7
-    val asciiPercentage = printableASCII.length.toDouble() / max(string.length, 7)
-    if(asciiPercentage > 0.8 && weirdASCII.isEmpty()) {
+    val asciiPercentage = printableASCII.length.toDouble() / string.length
+    val biasedAsciiPercentage = printableASCII.length.toDouble() / max(string.length, 7)
+    if((biasedAsciiPercentage > 0.8 || asciiPercentage == 1.0) && weirdASCII.isEmpty()) {
         Logger.log("mostly plausible ascii ($asciiPercentage): $string ${asciiPercentage-lengthBias}")
         return max(asciiPercentage - lengthBias, 0.0)
     }
@@ -78,7 +79,7 @@ fun looksLikeUtf16String(string: String, enableLengthBias: Boolean = true): Doub
 
     // CJK characters are the most likely to generate "randomly" and should usually not co-occur with non-CJK characters, excluding common ASCII
     val cjk = bins[0] + bins[1] + bins[2] + bins[7] + bins[10]
-    val nonCJKnonLatin = bins.sum() - cjk - bins[13] - bins[14]
+    val nonCJKnonLatin = bins.sum() - cjk - bins[13] - bins[14] - bins[binCount-1]
     val mixedCJKnonCJKPenalty = if(cjk > 0 && nonCJKnonLatin > 0) 1.0 else 0.0
 
     // han characters and hangul syllables make up for most codepoints - therefore both occurring in the same decode is likely for non-Unicode data
@@ -92,8 +93,8 @@ fun looksLikeUtf16String(string: String, enableLengthBias: Boolean = true): Doub
     // codepoints spread over many blocks / alphabets are a red flag
     val binCountPenalty = max(bins.count { it > 0 } - 3, 0)
 
-    val score = max(0.0, 1.0 + surrogatesBonus*0.25 + asciiPercentage/2 - rareCharactersPenalty*2 - mixedCJKnonCJKPenalty * 0.5 - mixedHanHangulPenalty*0.3 - binCountPenalty*0.25 - lengthBias)
-    Logger.log("surrogateBonus ${surrogatesBonus*0.25}, asciiPercentage ${asciiPercentage/2} rare ${-rareCharactersPenalty*2} mixedCJK ${-mixedCJKnonCJKPenalty*0.5}, mixHan ${-mixedHanHangulPenalty*0.3}, bins ${-binCountPenalty*0.25}, length ${-lengthBias}, final $score, string $string")
+    val score = max(0.0, 1.0 + surrogatesBonus*0.25 + biasedAsciiPercentage/2 - rareCharactersPenalty*2 - mixedCJKnonCJKPenalty * 0.5 - mixedHanHangulPenalty*0.3 - binCountPenalty*0.25 - lengthBias)
+    Logger.log("surrogateBonus ${surrogatesBonus*0.25}, asciiPercentage ${biasedAsciiPercentage/2} rare ${-rareCharactersPenalty*2} mixedCJK ${-mixedCJKnonCJKPenalty*0.5}, mixHan ${-mixedHanHangulPenalty*0.3}, bins ${-binCountPenalty*0.25}, length ${-lengthBias}, final $score, string $string")
 
     return score
 }
