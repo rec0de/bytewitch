5b15d245ea249c3271f0a60a6d6ecb3eb7112337
Leo <unconfigured@null.spigotmc.org>
2025-06-18 14:12:17 +0200

    some better readability, variable naming and comments

--- src/commonMain/kotlin/decoders/Kaitai.kt
+++ src/commonMain/kotlin/decoders/Kaitai.kt
@@ -16,8 +16,8 @@ external object JsYaml {
     fun dump(obj: dynamic): String
 }
 
-enum class displayStyle {
-    HEX, BINARY, NUMBER, STRING
+enum class DisplayStyle {
+    HEX, BINARY, SIGNED_INTEGER, UNSIGNED_INTEGER, FLOAT, STRING
 }
 
 // acts just like a MutableList except it also has the added features
@@ -37,52 +37,56 @@ class MutableListTree<T>(private val innerList: MutableList<T> = mutableListOf()
         private set
 }
 
-class Type(yamlStruct: dynamic, elementStruct: dynamic) {
-
+class Type(completeStruct: dynamic, currentElementStruct: dynamic) {
     var sizeInBits: Int = 0
     var sizeIsUntilEOS: Boolean = false
-    var type: String = elementStruct.type.toString()
+    var type: String = currentElementStruct.type.toString()
     var endianness: ByteOrder
-    var usedDisplayStyle: displayStyle = displayStyle.HEX
+    var usedDisplayStyle: DisplayStyle = DisplayStyle.HEX
     var subTypes: MutableList<Type> = mutableListOf<Type>()
 
+    fun parseBuiltinType() {
+        if (this.type == "strz") {  // TODO totally wrong, is actually a shortcut for type = str + terminator = 0. Other terminator bytes can exist
+            sizeIsUntilEOS = true
+            usedDisplayStyle = DisplayStyle.STRING
+        } else {
+            if (this.type.startsWith("s")) {  // signed int
+                sizeInBits = this.type.filter { it.isDigit() }.toInt() * 8
+                usedDisplayStyle = DisplayStyle.UNSIGNED_INTEGER
+            } else if (this.type.startsWith("u")) {  // unsigned int
+                sizeInBits = this.type.filter { it.isDigit() }.toInt() * 8
+                usedDisplayStyle = DisplayStyle.SIGNED_INTEGER
+            } else if (this.type.startsWith("f")) {  // float
+                sizeInBits = this.type.filter { it.isDigit() }.toInt() * 8
+                usedDisplayStyle = DisplayStyle.FLOAT
+            } else if (this.type.startsWith("b")) {  // binary
+                sizeInBits = this.type.filter { it.isDigit() }.toInt()
+                usedDisplayStyle = DisplayStyle.BINARY
+            } else {
+                throw RuntimeException()
+            }
+        }
+    }
+
     init {
-        if (yamlStruct.meta.endianness != undefined) {
-            endianness = yamlStruct.meta.endianness
+        if (completeStruct.meta.endianness != undefined) {
+            endianness = completeStruct.meta.endianness
         } else {
             endianness = ByteOrder.LITTLE
         }
 
-        if (elementStruct.size != undefined) {
-            sizeInBits = elementStruct.size * 8
+        if (currentElementStruct.size != undefined) {
+            sizeInBits = currentElementStruct.size * 8
         } else {
-            if (elementStruct["size-eos"]) {
+            if (currentElementStruct["size-eos"]) {
                 sizeIsUntilEOS = true
-            } else if (yamlStruct.types[this.type] == undefined) {  // should be its own if not else if, as size-eos can be made of subtypes
-                if (this.type == "strz") {  //totally wrong, is actually a shortcut for type = str + terminator = 0. Other terminator bytes can exist
-                    sizeIsUntilEOS = true
-                    usedDisplayStyle = displayStyle.STRING
-                } else {
-                    if (this.type.startsWith("s")) {  // signed int
-                        sizeInBits = this.type.filter { it.isDigit() }.toInt() * 8
-                        usedDisplayStyle = displayStyle.NUMBER
-                    } else if (this.type.startsWith("u")) {  // unsigned int
-                        sizeInBits = this.type.filter { it.isDigit() }.toInt() * 8
-                        usedDisplayStyle = displayStyle.NUMBER
-                    } else if (this.type.startsWith("f")) {  // float
-                        sizeInBits = this.type.filter { it.isDigit() }.toInt() * 8
-                        usedDisplayStyle = displayStyle.NUMBER
-                    } else if (this.type.startsWith("b")) {  // binary
-                        sizeInBits = this.type.filter { it.isDigit() }.toInt()
-                        usedDisplayStyle = displayStyle.BINARY
-                    } else {
-                        throw RuntimeException()
-                    }
-                }
+            } else if (completeStruct.types[this.type] == undefined) {  // TODO should be its own if not else if, as size-eos can be made of subtypes
+                // TODO could also be an imported type instead...
+                parseBuiltinType()
             } else {
                 sizeInBits = 0
-                for (subElementStruct in yamlStruct.types[this.type].seq) {
-                    var subType = Type(yamlStruct, subElementStruct)
+                for (subElementStruct in completeStruct.types[this.type].seq) {
+                    var subType = Type(completeStruct, subElementStruct)
                     subTypes.add(subType)
                     sizeInBits += subType.sizeInBits
                 }
@@ -92,18 +96,16 @@ class Type(yamlStruct: dynamic, elementStruct: dynamic) {
 }
 
 object Kaitai : ByteWitchDecoder {
-    override val name = ""
+    override val name = "Kaitai"
 
     override fun decode(data: ByteArray, sourceOffset: Int, inlineDisplay: Boolean): ByteWitchResult {
-        //val decodedResult: MutableMap<Any, Any> = mutableMapOf()
-
         val kaitaiInput = document.getElementById("kaitaiinput") as HTMLTextAreaElement
         val kaitaiYaml = JsYaml.load(kaitaiInput.value)
 
         return processSeq(kaitaiYaml.meta.id, null, kaitaiYaml, kaitaiYaml.seq, data.toBooleanArray(), sourceOffset)
     }
 
-    fun processSeq(id: String, parentKaitaiBytesList: MutableListTree<KaitaiElement>?, yamlStruct: dynamic, seqStruct: dynamic, data: BooleanArray, sourceOffsetInBits: Int) : KaitaiElement {
+    fun processSeq(id: String, parentBytesListTree: MutableListTree<KaitaiElement>?, completeStruct: dynamic, currentSeqStruct: dynamic, data: BooleanArray, sourceOffsetInBits: Int) : KaitaiElement {
         var currentOffsetInBits = 0
         /*
         Entweder data als ByteArray und Bitshiften
@@ -111,51 +113,50 @@ object Kaitai : ByteWitchDecoder {
         test[6..8] = 1
         test and 0b00000111 >> 0 = -> 101
         test and 0b00111000 >> 3 = -> 001
-        oder data als BooleanArray
+        oder data als BooleanArray so wie aktuell. Vermutlich inperformant, aber wohl gut genug
         */
-        val kaitaiBytesList = MutableListTree<KaitaiElement>()
-        kaitaiBytesList.parent = parentKaitaiBytesList
+        val bytesListTree = MutableListTree<KaitaiElement>()
+        bytesListTree.parent = parentBytesListTree
         //val types = mutableSetOf<Type>()
 
-        for (element in seqStruct) {
-            val type = Type(yamlStruct, element)
+        for (seqElement in currentSeqStruct) {
+            val type = Type(completeStruct, seqElement)
             if (type.sizeIsUntilEOS) {
                 type.sizeInBits = (data.size - currentOffsetInBits)
             }
 
-            val elementId = element.id
+            val elementId = seqElement.id
 
             var kaitaiElement : KaitaiElement
             val value = data.sliceArray(currentOffsetInBits .. currentOffsetInBits + type.sizeInBits -1)
             val sourceByteRange = Pair((currentOffsetInBits + sourceOffsetInBits).toFloat()/8, (sourceOffsetInBits + currentOffsetInBits + type.sizeInBits).toFloat()/8)
 
             if (type.subTypes.isNotEmpty()) {
-                kaitaiElement = processSeq(elementId, kaitaiBytesList, yamlStruct, yamlStruct.types[element.type].seq, value, sourceOffsetInBits + currentOffsetInBits)
+                kaitaiElement = processSeq(elementId, bytesListTree, completeStruct, completeStruct.types[seqElement.type].seq, value, sourceOffsetInBits + currentOffsetInBits)
             } else {
-                val endianness = ByteOrder.LITTLE
-                kaitaiElement = if (type.usedDisplayStyle == displayStyle.BINARY) {
+                kaitaiElement = if (type.usedDisplayStyle == DisplayStyle.BINARY) {
                     KaitaiBinary(
                         elementId,
-                        endianness,
+                        type.endianness,
                         value,
                         sourceByteRange
                     )
                 } else { //displayStyle.HEX as the fallback
                     KaitaiBytes(
                         elementId,
-                        endianness,
+                        type.endianness,
                         value,
                         sourceByteRange
                     )
                 }
             }
 
-            kaitaiBytesList.add(kaitaiElement)
+            bytesListTree.add(kaitaiElement)
 
             currentOffsetInBits += type.sizeInBits
         }
 
-        return KaitaiResult(id, kaitaiBytesList, Pair(sourceOffsetInBits.toFloat()/8, (data.size + sourceOffsetInBits).toFloat()/8))
+        return KaitaiResult(id, bytesListTree, Pair(sourceOffsetInBits.toFloat()/8, (data.size + sourceOffsetInBits).toFloat()/8))
     }
 
     override fun confidence(data: ByteArray): Double {
@@ -167,13 +168,13 @@ object Kaitai : ByteWitchDecoder {
 
 interface KaitaiElement : ByteWitchResult {
     val id: String
-    val kaitaiBytesList: MutableListTree<KaitaiElement>? get() = null
+    val bytesListTree: MutableListTree<KaitaiElement>? get() = null
     val value: BooleanArray? get() = null
 }
 
-class KaitaiResult(override val id: String, override val kaitaiBytesList: MutableListTree<KaitaiElement>, override val sourceByteRange: Pair<Float, Float>): KaitaiElement {
+class KaitaiResult(override val id: String, override val bytesListTree: MutableListTree<KaitaiElement>, override val sourceByteRange: Pair<Float, Float>): KaitaiElement {
     override fun renderHTML(): String {
-        return "<div class=\"generic roundbox\" $byteRangeDataTags>${id}(${kaitaiBytesList.joinToString("") { it.renderHTML() }})</div>"
+        return "<div class=\"generic roundbox\" $byteRangeDataTags>${id}(${bytesListTree.joinToString("") { it.renderHTML() }})</div>"
     }
 }
 
