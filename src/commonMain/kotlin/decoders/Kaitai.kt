393231686654010df4a2aa17806241ac9887fbef
Leo <>
2025-07-14 23:13:51 +0200

    fully pulling out byteOrder parsing out of Type() and instead using a single parseType method with later overriding of parameters Also a few bugfixes all around eos, type, etc also Working .terminator/type: strz

--- src/commonMain/kotlin/decoders/Kaitai.kt
+++ src/commonMain/kotlin/decoders/Kaitai.kt
@@ -2,6 +2,7 @@ package decoders
 
 import bitmage.ByteOrder
 import bitmage.hex
+import bitmage.indexOfFirstSubsequence
 import bitmage.padLeft
 import bitmage.toBooleanArray
 import bitmage.toByteArray
@@ -40,101 +41,15 @@ class MutableListTree<T>(private val innerList: MutableList<T> = mutableListOf()
         private set
 }
 
-class Type(val completeStruct: dynamic, val currentElementStruct: dynamic, val bytesListTree: MutableListTree<KaitaiElement>) {  // TODO: integrate Type Object and it's things into the seq parsing, as a lot of stuff from there is actually needed here
+class Type(seqElement : dynamic) {
+    var byteOrder: ByteOrder = ByteOrder.BIG
     var sizeInBits: UInt = 0u
     var sizeIsUntilEOS: Boolean = false
     var sizeIsUntilTerminator: Boolean = false
     var terminator: ByteArray? = null
-    var type: String = currentElementStruct.type.toString()
+    var type: String? = if (seqElement.type) {seqElement.type.toString()} else {null}
     var usedDisplayStyle: DisplayStyle = DisplayStyle.HEX
     var subTypes: MutableList<Type> = mutableListOf<Type>()
-
-    fun parseBuiltinType() {
-        if (type == "strz") {
-            usedDisplayStyle = DisplayStyle.STRING
-            sizeIsUntilTerminator = true
-            terminator = ByteArray(0x00)
-        } else if (type == "str") {
-            usedDisplayStyle = DisplayStyle.STRING
-            sizeIsUntilTerminator = true
-            terminator = currentElementStruct.terminator // TODO: Make proper method out of this. Could probably be a value defined somewhere else aswell :(
-        } else {
-            if (Regex("^s\\d+(le|be)?\$").matches(type)) {  // signed int
-                sizeInBits = type.filter { it.isDigit() }.toUInt() * 8u
-                usedDisplayStyle = DisplayStyle.UNSIGNED_INTEGER
-            } else if (Regex("^u\\d+(le|be)?\$").matches(type)) {  // unsigned int
-                sizeInBits = type.filter { it.isDigit() }.toUInt() * 8u
-                usedDisplayStyle = DisplayStyle.SIGNED_INTEGER
-            } else if (Regex("^f\\d+(le|be)?\$").matches(type)) {  // float
-                sizeInBits = type.filter { it.isDigit() }.toUInt() * 8u
-                usedDisplayStyle = DisplayStyle.FLOAT
-            } else if (Regex("^b\\d+(le|be)?\$").matches(type)) {  // binary
-                sizeInBits = type.filter { it.isDigit() }.toUInt()
-                usedDisplayStyle = DisplayStyle.BINARY
-            } else {
-                throw RuntimeException("Attempted to parse as builtin type $type but that doesn't seem to be a valid type")
-            }
-        }
-    }
-
-    // TODO: See the comment above, this is a duplicate of the one in Kaitai.kt
-    fun parseValue(value: dynamic, bytesListTree: MutableListTree<KaitaiElement>) : BooleanArray {
-        val flattenedArray = js("[value].flat(2)")  // handle both cases of 0x0a as well as [0x0a]
-        var fullyFlatArray = booleanArrayOf()
-        for (element in flattenedArray) {
-            if (element is Int) {
-                fullyFlatArray += (element as Int).toByte().toBooleanArray()
-            } else {
-                try {
-                    fullyFlatArray += parseReference(element, bytesListTree)
-                } catch (e: Exception) {
-                    for (i in 0..element.length - 1) {
-                        fullyFlatArray += (element.charCodeAt(i) as Int).toByte().toBooleanArray()
-                    }
-                }
-            }
-        }
-        return fullyFlatArray
-    }
-
-    // TODO: See the comment above, this is a duplicate of the one in Kaitai.kt
-    fun parseReference(reference: String, bytesListTree: MutableListTree<KaitaiElement>): BooleanArray {
-        // if it is not a valid reference, i.e. there exists no element in the seq with the id @param: reference, then an exception is thrown
-        if (reference.startsWith("_root.")) //_root.magic
-            return parseReference(reference.removePrefix("_root."), bytesListTree.root!!)
-        else if (reference.startsWith("_parent."))
-            return parseReference(reference.removePrefix("_parent."), bytesListTree.parent!!)
-        else (
-                if (reference.contains("."))
-                    return parseReference(reference.substringAfter("."), bytesListTree.find { it.id == reference.substringBefore(".") }!!.bytesListTree!!)
-                else
-                    return bytesListTree.find { it.id == reference }!!.value
-                )
-    }
-
-    init {
-        if (currentElementStruct.size != undefined) {
-            val parsedValue = parseValue(currentElementStruct.size, bytesListTree)
-            sizeInBits = parsedValue.toByteArray().toUInt(ByteOrder.BIG) * 8u
-        }
-        if (currentElementStruct.contents != undefined) {
-            val tmp = parseValue(currentElementStruct.contents, bytesListTree)
-            sizeInBits = tmp.size.toUInt()
-            console.log(sizeInBits)
-        } else if (currentElementStruct["size-eos"]) {
-            sizeIsUntilEOS = true
-        } else if ((completeStruct.types != undefined) && completeStruct.types[this.type] != undefined) {  // parse subtypes
-            sizeInBits = 0u
-            for (subElementStruct in completeStruct.types[this.type].seq) {
-                var subType = Type(completeStruct, subElementStruct, bytesListTree)
-                subTypes.add(subType)
-                sizeInBits += subType.sizeInBits
-            }
-        } else if (currentElementStruct.type != undefined){  // TODO should be its own if not else if, as size-eos can be made of subtypes
-            // TODO could also be an imported type instead...
-            parseBuiltinType()
-        }
-    }
 }
 
 // TODO[IMPORTANT]: Move the ByteWitchDecoder into a companion object as the parse methods are static and do not need an instance. See the other decoders for examples
@@ -143,18 +58,13 @@ class Kaitai(val kaitaiName: String, val kaitaiStruct: String) : ByteWitchDecode
     lateinit var completeStruct: JsYaml
 
     override fun decode(data: ByteArray, sourceOffset: Int, inlineDisplay: Boolean): ByteWitchResult {
-        /*
-        val kaitaiInput = document.getElementById("kaitaiinput") as HTMLTextAreaElement
-        val kaitaiYaml = JsYaml.load(kaitaiInput.value)*/
-
         val kaitaiYaml = JsYaml.load(kaitaiStruct)
         completeStruct = kaitaiYaml
         val result = try {  // JS Exceptions don't get simply logged to console but instead trigger the big red overlay. We convert JS Errors to Kotlin Exceptions here
             processSeq(kaitaiYaml.meta.id, null, kaitaiYaml, kaitaiYaml, data.toBooleanArray(), sourceOffset)
         } catch (e: dynamic) {
-            throw Exception("Unexpected JS Exception has been thrown:\n$e")
+            throw Exception("Unexpected Exception has been thrown:\n$e")
         }
-
         return result
     }
 
@@ -173,7 +83,7 @@ class Kaitai(val kaitaiName: String, val kaitaiStruct: String) : ByteWitchDecode
     }
 
     fun parseValue(value: dynamic, bytesListTree: MutableListTree<KaitaiElement>) : BooleanArray {
-        val flattenedArray = js("[value].flat(2)")  // handle both cases of 0x0a as well as [0x0a]
+        val flattenedArray = js("[value].flat(2)")  // handle both cases of 0x0a as well as [0x0a]. // TODO Can someone find an elegant way without javascript?
         var fullyFlatArray = booleanArrayOf()
         for (element in flattenedArray) {
             if (element is Int) {
@@ -207,15 +117,6 @@ class Kaitai(val kaitaiName: String, val kaitaiStruct: String) : ByteWitchDecode
                 return false
             }
             return true
-//            for (i in 0..value.toByteArray().size-1) {
-//                if ((valid.min != undefined) && (value.toByteArray()[i].toUByte() < parseValue(valid.min, bytesListTree).toByteArray()[i].toUByte())) {
-//                    return false
-//                }
-//                //if ((valid.max != undefined) && (value.toByteArray()[i].toUByte() > parseValue(valid.max, bytesListTree).toByteArray().padToSize(value.toByteArray().size, endianness)[i].toUByte())) {
-//                if ((valid.max != undefined) && (value.toByteArray()[i].toUByte() > parseValue(valid.max, bytesListTree).toByteArray()[i].toUByte())) {
-//                    return false
-//                }
-//            }
         } else if (seqElement.valid["any-of"] != undefined) {
             for (i in 0..seqElement.valid["any-of"].length-1) {
                 if (parseValue(seqElement.valid["any-of"][i], bytesListTree).contentEquals(dataBytes)) {
@@ -248,11 +149,15 @@ class Kaitai(val kaitaiName: String, val kaitaiStruct: String) : ByteWitchDecode
     }
 
     // TODO: add support for imports
-    /*
-        check if there is a fitting custom type defined in the current or global scope
-        @param path: might be just a type or prefixed by a path via "::"
+    /**
+     * check if there is a fitting custom type defined in the current or global scope
+     * @param path: might be just a type or prefixed by a path via "::"
+     * @return null if type doesn't exist in any scope or appropriate scope if it does
      */
-    fun getCustomType(currentScopeStruct: dynamic, path: String) : dynamic {
+    fun getCustomType(currentScopeStruct: dynamic, path: String?) : dynamic {
+        if (path == null) {
+            return null
+        }
         try {
             return parseStaticPath(currentScopeStruct, path)
         } catch (e: dynamic) { // catch js exceptions
@@ -276,22 +181,78 @@ class Kaitai(val kaitaiName: String, val kaitaiStruct: String) : ByteWitchDecode
                     byteOrder = ByteOrder.BIG
                 } else if (seqElement.type.endsWith("le")) {
                     byteOrder = ByteOrder.LITTLE
-                } else if (seqElement.type.startsWith("str")) {
-                    if (seqElement.encoding != undefined) {
-                        if (seqElement.emcoding.endsWith("be")) {
-                            byteOrder = ByteOrder.BIG
-                        } else if (seqElement.encoding.endsWith("le")) {
-                            byteOrder = ByteOrder.LITTLE
-                        }
-                    }
                 }
             }
         }
         return byteOrder
     }
 
+    fun parseBuiltinType(seqElement : dynamic, bytesListTree: MutableListTree<KaitaiElement>, type: Type) : Type {
+        if (type.type == null) {
+            throw RuntimeException("Attempted to parse as builtin type null which is always invalid")
+        }
+        if (type.type == "strz") {
+            type.usedDisplayStyle = DisplayStyle.STRING
+            type.sizeIsUntilTerminator = true
+            type.terminator = byteArrayOf(0x00)
+        } else if (type.type == "str") {
+            type.usedDisplayStyle = DisplayStyle.STRING
+            if (type.terminator != undefined) {
+                type.sizeIsUntilTerminator = true
+                type.terminator = parseValue(seqElement.terminator, bytesListTree).toByteArray() // TODO: Make proper method out of this. Could probably be a value defined somewhere else aswell :(
+            }
+        } else {
+            if (Regex("^s\\d+(le|be)?$").matches(type.type!!)) {  // signed int
+                type.sizeInBits = type.type!!.filter { it.isDigit() }.toUInt() * 8u
+                type.usedDisplayStyle = DisplayStyle.UNSIGNED_INTEGER
+            } else if (Regex("^u\\d+(le|be)?$").matches(type.type!!)) {  // unsigned int
+                type.sizeInBits = type.type!!.filter { it.isDigit() }.toUInt() * 8u
+                type.usedDisplayStyle = DisplayStyle.SIGNED_INTEGER
+            } else if (Regex("^f\\d+(le|be)?$").matches(type.type!!)) {  // float
+                type.sizeInBits = type.type!!.filter { it.isDigit() }.toUInt() * 8u
+                type.usedDisplayStyle = DisplayStyle.FLOAT
+            } else if (Regex("^b\\d+(le|be)?$").matches(type.type!!)) {  // binary
+                type.sizeInBits = type.type!!.filter { it.isDigit() }.toUInt()
+                type.usedDisplayStyle = DisplayStyle.BINARY
+            } else {
+                throw RuntimeException("Attempted to parse as builtin type ${type.type} but that doesn't seem to be a valid type")
+            }
+        }
+        return type
+    }
+
+    fun parseType(currentScopeStruct: dynamic, seqElement: dynamic, bytesListTree: MutableListTree<KaitaiElement>) : Type {
+        var type = Type(seqElement)
+        type.byteOrder = parseByteOrder(currentScopeStruct, seqElement, bytesListTree)
+        if (seqElement.size != undefined) {
+            val parsedValue = parseValue(seqElement.size, bytesListTree)
+            type.sizeInBits = parsedValue.toByteArray().toUInt(ByteOrder.BIG) * 8u
+        }
+        if (seqElement.contents != undefined) {
+            type.sizeInBits = parseValue(seqElement.contents, bytesListTree).size.toUInt()
+        }
+        if (seqElement["size-eos"] != undefined) {
+            type.sizeIsUntilEOS = seqElement["size-eos"]
+        }
+        if (seqElement.terminator != undefined) {
+            type.terminator = parseValue(seqElement.terminator, bytesListTree).toByteArray()
+            type.sizeIsUntilTerminator = true
+        }
+        if (getCustomType(currentScopeStruct, type.type) != null) {  // parse custom type aka subtypes
+            type.sizeInBits = 0u
+            for (subElementStruct in getCustomType(currentScopeStruct, type.type).seq) {
+                val subType = parseType(completeStruct, subElementStruct, bytesListTree)
+                type.subTypes.add(subType)
+                type.sizeInBits += subType.sizeInBits
+            }
+        } else if (type.type != null) {  // parse builtin type
+            type = parseBuiltinType(seqElement, bytesListTree, type)
+        }
+        return type
+    }
+
     fun processSeq(id: String, parentBytesListTree: MutableListTree<KaitaiElement>?, completeStruct: dynamic, currentScopeStruct: dynamic, data: BooleanArray, sourceOffsetInBits: Int) : KaitaiElement {
-        var currentOffsetInBits = 0
+        var currentOffsetInBits = 0 //TODO Wrong
         /*
         Entweder data als ByteArray und Bitshiften
         var test = 13  -> 1101
@@ -299,6 +260,9 @@ class Kaitai(val kaitaiName: String, val kaitaiStruct: String) : ByteWitchDecode
         test and 0b00000111 >> 0 = -> 101
         test and 0b00111000 >> 3 = -> 001
         oder data als BooleanArray so wie aktuell. Vermutlich inperformant, aber wohl gut genug
+        size: 1 == 1 ? 2 : 3
+        size: (1 == 1) ? 2 : 3
+        size: 1 == (1 ? 2 : 3) <-
         */
         val bytesListTree = MutableListTree<KaitaiElement>()
         bytesListTree.parent = parentBytesListTree
@@ -314,15 +278,23 @@ class Kaitai(val kaitaiName: String, val kaitaiStruct: String) : ByteWitchDecode
                 ByteOrder.LITTLE
             }
         }
-        //val types = mutableSetOf<Type>()
 
         for (seqElement in currentScopeStruct.seq) {
-            val byteOrder : ByteOrder = parseByteOrder(currentScopeStruct, seqElement, bytesListTree)
-
-            val type = Type(completeStruct, seqElement, bytesListTree)
+            val type = parseType(currentScopeStruct, seqElement, bytesListTree)
             if (type.sizeIsUntilEOS) {
                 type.sizeInBits = (data.size - currentOffsetInBits).toUInt()
             }
+            if (type.sizeIsUntilTerminator) {
+                val dataAsByteArray = data.toByteArray()  // technically not ideal as we don't check for bytealignement, but right after we throw away all results if it's not byte aligned
+                type.sizeInBits = (dataAsByteArray.sliceArray(currentOffsetInBits / 8 .. dataAsByteArray.size-1).indexOfFirstSubsequence(type.terminator).toUInt() + type.terminator!!.size.toUInt()) * 8u
+            }
+
+            // if we are operating on non byte aligned data, but we don't have a binary data type, something is very wrong
+            if (((((currentOffsetInBits + sourceOffsetInBits) % 8) != 0) && (type.usedDisplayStyle != DisplayStyle.BINARY))
+                && type.subTypes.isEmpty()) {  // if it has subtypes we ignore the problem for now and we'll see inside the subtype again
+               throw RuntimeException("Cannot have a non binary type that starts in the middle of a byte")
+            }
+
             val elementId = seqElement.id
 
             var kaitaiElement : KaitaiElement
@@ -330,7 +302,7 @@ class Kaitai(val kaitaiName: String, val kaitaiStruct: String) : ByteWitchDecode
             // flip bytes in case byteOrder is little and therefore different order than kotlin
             if (type.subTypes.isEmpty() // no subtypes
                 && (type.usedDisplayStyle == DisplayStyle.FLOAT || type.usedDisplayStyle == DisplayStyle.SIGNED_INTEGER || type.usedDisplayStyle == DisplayStyle.UNSIGNED_INTEGER) // makes sense to flip
-                && byteOrder == ByteOrder.LITTLE) { // little needs flipping, big doesn't need it anyways
+                && type.byteOrder == ByteOrder.LITTLE) { // little needs flipping, big doesn't need it anyways
                 value = value.toByteArray().reversedArray().toBooleanArray()
             }
 
@@ -343,7 +315,7 @@ class Kaitai(val kaitaiName: String, val kaitaiStruct: String) : ByteWitchDecode
                 kaitaiElement = if (type.usedDisplayStyle == DisplayStyle.BINARY) {
                     KaitaiBinary(
                         elementId,
-                        byteOrder,
+                        type.byteOrder,
                         value,
                         sourceByteRange,
                         sourceRangeBitOffset
@@ -351,7 +323,7 @@ class Kaitai(val kaitaiName: String, val kaitaiStruct: String) : ByteWitchDecode
                 } else if (type.usedDisplayStyle == DisplayStyle.STRING) {
                     KaitaiString(
                         elementId,
-                        byteOrder,
+                        type.byteOrder,
                         value,
                         sourceByteRange,
                         sourceRangeBitOffset
@@ -359,7 +331,7 @@ class Kaitai(val kaitaiName: String, val kaitaiStruct: String) : ByteWitchDecode
                 } else { // displayStyle.HEX as the fallback (even if it's a known type like int or whatever
                     KaitaiBytes(
                         elementId,
-                        byteOrder,
+                        type.byteOrder,
                         value,
                         sourceByteRange,
                         sourceRangeBitOffset
@@ -419,7 +391,7 @@ class KaitaiBytes(override val id: String, override var endianness: ByteOrder, o
 
 class KaitaiInteger(override val id: String, override var endianness: ByteOrder, override val value: BooleanArray, override val sourceByteRange: Pair<Int, Int>): KaitaiElement {
     override fun renderHTML(): String {
-        return "<div class=\"generic roundbox\" $byteRangeDataTags>${id}(${value.toByteArray().toInt(ByteOrder.BIG)})h</div>"
+        return "<div class=\"generic roundbox\" $byteRangeDataTags>${id}(${value.toByteArray().toInt(ByteOrder.BIG)})s</div>"
     }
 }
 
