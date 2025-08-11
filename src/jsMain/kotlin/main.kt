1613342f69406676395a088ad1adcbe21229f45b
Benedikt Hock <benedikt.hock@stud.tu-darmstadt.de>
2025-08-11 22:46:43 +0200

    Remove redundant LayoutManager update call

--- src/jsMain/kotlin/main.kt
+++ src/jsMain/kotlin/main.kt
@@ -26,7 +26,6 @@ fun main() {
         KaitaiUI.loadKaitaiStructsFromStorage()
 
         LayoutManager
-        LayoutManager.updateLayout()
 
         val input = document.getElementById("data") as HTMLTextAreaElement
         val decodeBtn = document.getElementById("decode") as HTMLButtonElement
